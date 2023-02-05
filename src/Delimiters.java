86bd60b8776034a6ae76305d94261542e3c3f2e6
Sneeky Stryder <ifazilov3584@bths.edu>
2023-02-05 12:05:17 -0500

    lab done

---
+++ src/Delimiters.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class Delimiters
+{
+    /** The open and close delimiters **/
+    private String openDel;
+    private String closeDel;
+
+    /** Constructs a Delimiters object where open is the open delimiter and close is the
+     *  close delimiter.
+     *  Precondition: open and close are non-empty strings.
+     */
+    public Delimiters(String open, String close)
+    {
+        openDel = open;
+        closeDel = close;
+    }
+    /** Returns an ArrayList of delimiters from the array tokens, as described in part (a). */
+    public ArrayList<String> getDelimitersList(String[] tokens)
+    {
+        ArrayList<String> delimiters = new ArrayList<>();
+        for (String string : tokens) {
+            if (string.equals(openDel) || string.equals(closeDel)) {
+                delimiters.add(string);
+            }
+        }
+        return delimiters;
+    }
+
+    /** Returns true if the delimiters are balanced and false otherwise, as described in part (b).
+     *  Precondition: delimiters contains only valid open and close delimiters.
+     */
+    public boolean isBalanced(ArrayList<String> delimiters)
+    {
+        int openCount = 0;
+        int closeCount = 0;
+        for (String string : delimiters) {
+            if (string.equals(openDel)) {
+                openCount++;
+            }
+            else {
+                closeCount++;
+            }
+            if (closeCount > openCount) {
+                return false;
+            }
+        }
+        return openCount == closeCount;
+    }
+}
