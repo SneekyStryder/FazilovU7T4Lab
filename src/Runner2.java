0370528d6a82f37858025ba25e506d815bb6b2ee
SneekyStryder <106488424+SneekyStryder@users.noreply.github.com>
2023-02-07 12:31:29 -0500

    Create Runner2.java

---
+++ src/Runner2.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Runner2 {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("Jesse");
+        names.add("Bart");
+        names.add("David");
+        names.add("Abigail");
+        names.add("Frank");
+        names.add("Steph");
+        names.add("Thomas");
+        names.add("Jimmy");
+        names.add("Joe");
+        names.add("Klein");
+        names.add("Daisy");
+
+        System.out.println("BEFORE: " + names);
+
+        // write a loop below to traverse names and REMOVE each name that
+        // is exactly 5 letters
+        for (int i = 0; i < names.size(); i++) {
+            if (names.get(i).length() == 5) {
+                names.remove(i);
+                i--;
+            }
+        }
+
+
+        System.out.println("AFTER: " + names);
+
+    }
+}
